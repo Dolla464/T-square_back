1f01776e44387c4c32c1989736a73fd94dcb5824
Adel Moneem <dolla464@gmail.com>
2026-04-22 02:45:18 +0200

    my edits

--- app/Http/Resources/User/Instructors/InstructorResource.php
+++ app/Http/Resources/User/Instructors/InstructorResource.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Resources;
+namespace App\Http\Resources\User\Instructors;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
@@ -15,7 +15,7 @@ class InstructorResource extends JsonResource
                                 : null,
             'fullname' => $this->full_name,
             'field' => $this->field,
-            'email' => $this->whenLoaded('user', fn () => $this->user->email),
+            'email' => $this->user?->email ?? null,
             'insta_url' => $this->insta_url,
             'linkedin_url' => $this->linkedin_url,
             'facebook_url' => $this->facebook_url,
