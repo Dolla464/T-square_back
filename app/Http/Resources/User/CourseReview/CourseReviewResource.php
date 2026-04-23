d66facb6238428eff99afdc4bbe02bfed8b4ce09
Aln3san <anas.aln3san@gamil.com>
2026-04-23 16:23:42 +0200

    Add CourseReviewController and latest reviews route to API

---
+++ app/Http/Resources/User/CourseReview/CourseReviewResource.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Resources\User\CourseReview;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CourseReviewResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id'              => $this->id,
+            'rating'          => $this->rating,
+            'overall_comment' => $this->overall_comment,
+ 
+            'student' => [
+                'full_name' => $this->student?->full_name,
+                'avatar'    => $this->student?->avatar,
+            ],
+ 
+            'course' => [
+                'title' => $this->course?->title,
+            ],
+ 
+            'instructor' => [
+                'full_name' => $this->instructor?->full_name,
+            ],
+        ];
+
+    }
+}
